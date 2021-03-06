4364a36c7c11b287a8c8be3a8749bb7263b7479d
karigirwa <happyka52@gmail.com>
2021-03-06 12:38:19 +0200

    react-validation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
-import Register from './components/Register'
-import Profile from './components/Profile'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/"  exact component={Register} />
-          <Route path="/profile"  component={Profile} />
+          <Route path="/" exact component={Register} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </div>
     </Router>
